d64b5ac8b02ef750ab76a4d9695f7352162120c3
L <89287469+eboy11398@users.noreply.github.com>
2021-08-28 10:14:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function rev() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function show() {
+    document.getElementById("white").style.display = 'block';  
+}
+
+function hide() {
+    document.getElementById("white").style.display = 'none';
+}
+
